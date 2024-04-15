44146ae1ead9787e96fb3f124c890e6a4bb58204
Ethan Davis <121524320+ethandav@users.noreply.github.com>
2024-04-15 13:23:24 -0700

    WebHello initial commit

---
+++ webhello/src/main.rs
@@ -0,0 +1,35 @@
+use std::net::{Ipv4Addr, SocketAddrV4, TcpListener, TcpStream};
+use std::io::{BufRead, Write, BufReader};
+
+
+fn discard_request(tcp_stream: &mut TcpStream) {
+    let mut reader = BufReader::new(tcp_stream);
+    let mut line = String::new();
+    loop {
+        reader.read_line(&mut line).unwrap();
+        let trimmed = line.trim_end();
+        if trimmed.is_empty() {
+            return;
+        }
+        eprintln!("{}", trimmed);
+        line.clear();
+    }
+}
+
+fn main() {
+    let localhost = Ipv4Addr::new(127, 0, 0, 1);
+    let socket_addr = SocketAddrV4::new(localhost, 3000);
+    eprintln!("server starts: {}", socket_addr);
+    let tcp_listener = TcpListener::bind(socket_addr).unwrap();
+    loop {
+        let (mut tcp_stream, addr) = tcp_listener.accept().unwrap();
+        eprintln!("connection from {}", addr);
+        discard_request(&mut tcp_stream);
+        let body: &str = "<html><head><meta charset=\"UTF-8\"/></head><body><em>hello world🦀</em></body></html>";
+        write!(tcp_stream, "HTTP/1.0 200 OK\r\n").unwrap();
+        write!(tcp_stream, "Content-Type: text/html;charset='UTF-8'\r\n").unwrap();
+        write!(tcp_stream, "\r\n").unwrap();
+        write!(tcp_stream, "{}", body).unwrap();
+        tcp_stream.flush().unwrap();
+    }
+}
